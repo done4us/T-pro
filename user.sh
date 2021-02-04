63d8389f2c844be6c89f7b117c47c5a43c06d57f
Dumped One <77003768+dumpedone5665@users.noreply.github.com>
2021-02-05 00:07:10 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -5,3 +5,5 @@ echo "PS1='\[\e[1;34m
 \a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
 \a├─[\033[1;32m\w\033[1;34m]\033[1;34m
 \[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\][~]:#\[\e[1;32m\] ' "
+
+cat "ash.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
