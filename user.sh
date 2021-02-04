0a72bfdaa0980b524c510111412b495f522bf6a7
Dumped One <77003768+dumpedone5665@users.noreply.github.com>
2021-02-05 00:34:12 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,9 +1,15 @@
+echo
+read -p $'\e[36m>>\e[33m Enter Your Terminal Name:\e[32m ' varterm
+echo
+
 # custom PS1, validating user input and user define variable
 
 echo "PS1='\[\e[1;34m
 \a┌──\a─T─I─M─E─\a──┐\033[1;34m\a┌──\a─D─A─T─E─\a───>\033[1;34m
 \a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
 \a├─[\033[1;32m\w\033[1;34m]\033[1;34m
-\[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\][~]:#\[\e[1;32m\] ' "
+\[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\][~]:#\[\e[1;32m\] ' " > ash.txt 
+
+cat "user.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
 cat "ash.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
