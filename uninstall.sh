671a80b0dec9f1759ba0f8642893c6ee3a868e84
Done4us <77003768+done4us@users.noreply.github.com>
2021-05-29 15:01:28 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -10,15 +10,25 @@ echo "command_not_found_handle() {
 }
 PS1='\033[1;93m\w\[ \033[1;92m :->>> '" > bash.bashrc
 echo "Welcome to Termux!
+
 Wiki:            https://wiki.termux.com
 Community forum: https://termux.com/community
-IRC channel:     #termux on freenode
 Gitter chat:     https://gitter.im/termux/termux
-Mailing list:    termux+subscribe@groups.io
-Search packages:   pkg search <query>
-Install a package: pkg install <package>
-Upgrade packages:  pkg upgrade
-Learn more:        pkg help" > motd
+IRC channel:     #termux on freenode
+
+Working with packages:
+
+ * Search packages:   pkg search <query>
+ * Install a package: pkg install <package>
+ * Upgrade packages:  pkg upgrade
+
+Subscribing to additional repositories:
+
+ * Root:     pkg install root-repo
+ * Unstable: pkg install unstable-repo
+ * X11:      pkg install x11-repo
+
+Report issues at https://termux.com/issues" > motd
 cd
 clear
 # Change fonts
@@ -37,8 +47,7 @@ clear
 cat "DejaVuSerifCondensed-Italic.ttf" > ~/.termux/font.ttf
 clear
 
-cd /$HOME
-clear
+
 figlet done4us | lolcat
 
 echo
